a2eb804fe4314b25d8f291f51c9d1df06295dfa6
Рамазан <you@example.com>
2024-03-29 16:26:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// let text = document.querySelector('textarea')
+// let count = document.querySelector('.count')
+
+
+
+// text.addEventListener('keydown', (e)=>{
+//     if(e.target.value.length === 20 || e.repeat === 20){
+//         if(e.target.value.match(/[a-zA-Z]/)){
+//             alert('Вы написали 20 символов на английском')
+//         }else{
+//             alert('Вы написали 20 символов на русском')
+//         }
+//     }
+// })
+
+// text.addEventListener('keydown', (e)=>{
+//     count.innerHTML = e.target.value.length 
+// })
+
+// let text = document.querySelector('textarea')
+// let p = document.querySelector('p')
+
+// text.addEventListener('keydown', (e)=>{
+//     if(text.value.match(/[a-zA-Z]/) ){
+//         p.innerHTML = `You wrote ${text.value.split(' ').length} words in english`
+//     }else if(e.target.value.match(/[а-яА-Я]/)){
+//         p.innerHTML = `Вы написали ${text.value.split(' ').length} слов на русском языке`
+//     }
+// })
